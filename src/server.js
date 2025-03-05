cd7a8559fd64143d79e9c38a8b907db5847c4868
unknown <jonathanjairxd02@gmail.com>
2025-03-05 08:57:29 -0500

    first commit

---
+++ src/server.js
@@ -0,0 +1,33 @@
+// Requerir los módulos
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors';
+
+// Inicializaciones
+const app = express()
+dotenv.config()
+
+// Configuraciones 
+app.set('port',process.env.port || 3000)
+app.use(cors())
+
+// Middlewares 
+app.use(express.json())
+
+
+// Variables globales
+
+
+// Rutas 
+app.get('/',(req,res)=>{
+    res.send("Server on")
+})
+
+
+
+// Rutas no encontradas
+app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
+
+
+// Exportar la instancia de express por medio de app
+export default  app
